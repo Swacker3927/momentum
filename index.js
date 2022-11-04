be44c6b832d6175b2f4594a2fbbaae5293c86037
Taewoo-Lee <xodn3130@gmail.com>
2022-11-04 20:13:42 +0900

    랜덤 사진 꾸미기

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
   renderCurrentTime();
   renderQuote();
+  renderRandomImage();
 });
 
+//현재 시간
 function renderCurrentTime() {
   let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
   fetch(url)
@@ -13,6 +15,7 @@ function renderCurrentTime() {
     });
 }
 
+// 명언
 function renderQuote() {
   let url = `https://api.quotable.io/random`;
   fetch(url)
@@ -23,4 +26,17 @@ function renderQuote() {
       $('#content').text(content);
       $('#author').text(author);
     });
+}
+
+// 랜덤 사진 꾸미기
+function renderRandomImage() {
+  let imageList = [];
+  // 이미지 개수를 변경하려면 i=5의 값을 이미지 개수만큼 바꿔주세요!
+  for (i = 0; i < 5; i++) {
+    imageList.push(i);
+  }
+  let imageListLength = imageList.length;
+  let randomImage = Math.floor(Math.random() * imageListLength) + 1;
+  randomImage = `images/${randomImage}.jpg`;
+  $(document.body).css('background-image', `url(${randomImage})`);
 }
